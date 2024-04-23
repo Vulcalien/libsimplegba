fcdeee3251872049f25e5959844e338126046944
Vulcalien <vulcalien@vulcalien.net>
2024-04-23 14:24:59 +0200

    Background: simplify interface and implementation of the module
    Instead of having 'struct Background' describe a collection of pointers
    related to each of the four GBA backgrounds, the structure now describes
    a logical background characterized by its attributes. This removes the
    need for 'struct background_Config' to exist.
    
    The list of registers has now been distributed among the inline
    functions ('background_config' and 'background_set_offset'), so that
    they do not need to be stored in a structure.

--- include/background.h
+++ include/background.h
@@ -17,22 +17,26 @@
 
 #include "base.h"
 
+#define BACKGROUND_COUNT (4)
+
+#define BG0_CONTROL ((vu16 *) 0x04000008)
+#define BG1_CONTROL ((vu16 *) 0x0400000a)
+#define BG2_CONTROL ((vu16 *) 0x0400000c)
+#define BG3_CONTROL ((vu16 *) 0x0400000e)
+
+#define BG0_XOFFSET ((vu16 *) 0x04000010)
+#define BG0_YOFFSET ((vu16 *) 0x04000012)
+
+#define BG1_XOFFSET ((vu16 *) 0x04000014)
+#define BG1_YOFFSET ((vu16 *) 0x04000016)
+
+#define BG2_XOFFSET ((vu16 *) 0x04000018)
+#define BG2_YOFFSET ((vu16 *) 0x0400001a)
+
+#define BG3_XOFFSET ((vu16 *) 0x0400001c)
+#define BG3_YOFFSET ((vu16 *) 0x0400001e)
+
 struct Background {
-    vu16 *control;
-
-    struct {
-        vu16 *x;
-        vu16 *y;
-    } offset;
-};
-
-#define BG0 (&backgrounds[0])
-#define BG1 (&backgrounds[1])
-#define BG2 (&backgrounds[2])
-#define BG3 (&backgrounds[3])
-extern const struct Background backgrounds[4];
-
-struct background_Config {
     // Behavior
     u32 priority : 2;
     u32 overflow : 1; // (only BG2/BG3) 0 = transparent, 1 = wraparound
@@ -50,20 +54,38 @@ struct background_Config {
 };
 
 ALWAYS_INLINE
-inline void background_config(const struct Background *bg,
-                              const struct background_Config *config) {
-    *(bg->control) = config->priority   << 0  |
-                     config->tileset    << 2  |
-                     config->mosaic     << 6  |
-                     config->color_mode << 7  |
-                     config->tilemap    << 8  |
-                     config->overflow   << 13 |
-                     config->size       << 14;
+inline void background_config(const struct Background *config, u32 id) {
+    if(id >= BACKGROUND_COUNT)
+        return;
+
+    vu16 *control_registers[BACKGROUND_COUNT] = {
+        BG0_CONTROL, BG1_CONTROL, BG2_CONTROL, BG3_CONTROL
+    };
+
+    *control_registers[id] = config->priority   << 0  |
+                             config->tileset    << 2  |
+                             config->mosaic     << 6  |
+                             config->color_mode << 7  |
+                             config->tilemap    << 8  |
+                             config->overflow   << 13 |
+                             config->size       << 14;
 }
 
 ALWAYS_INLINE
-inline void background_set_offset(const struct Background *bg,
-                                  u16 x, u16 y) {
-    *(bg->offset.x) = x;
-    *(bg->offset.y) = y;
+inline void background_set_offset(u16 x, u16 y, u32 id) {
+    if(id >= BACKGROUND_COUNT)
+        return;
+
+    struct {
+        vu16 *x;
+        vu16 *y;
+    } offset_registers[BACKGROUND_COUNT] = {
+        { BG0_XOFFSET, BG0_YOFFSET },
+        { BG1_XOFFSET, BG1_YOFFSET },
+        { BG2_XOFFSET, BG2_YOFFSET },
+        { BG3_XOFFSET, BG3_YOFFSET }
+    };
+
+    *(offset_registers[id].x) = x;
+    *(offset_registers[id].y) = y;
 }
