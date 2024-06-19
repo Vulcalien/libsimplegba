028112e0219f4d43d8ee1a4e12e80c41922ed8af
Vulcalien <vulcalien@vulcalien.net>
2024-06-19 21:27:03 +0200

    crt0: change formatting and add notes to match style of other files
    Also, the order in which sections are filled has changed (IWRAM first,
    EWRAM second) and no arguments are passed to 'AgbMain'.

--- src/crt0.s
+++ src/crt0.s
@@ -1,4 +1,4 @@
-@ Copyright 2022-2023 Vulcalien
+@ Copyright 2022-2024 Vulcalien
 @
 @ This program is free software: you can redistribute it and/or modify
 @ it under the terms of the GNU General Public License as published by
@@ -20,60 +20,61 @@
 .global start_vector
 .arm
 start_vector:
-        @ Set SP_irq
-        mov     r0, #0x12               @ Switch to IRQ Mode
-        msr     cpsr, r0
-        ldr     sp, =__sp_irq
+    @ switch to IRQ mode and set SP_irq
+    mov     r0, #0x12
+    msr     cpsr, r0
+    ldr     sp, =__sp_irq
 
-        @ Set SP_usr
-        mov     r0, #0x1f               @ Switch to System Mode
-        msr     cpsr, r0
-        ldr     sp, =__sp_usr
+    @ switch to System mode and set SP_usr
+    mov     r0, #0x1f
+    msr     cpsr, r0
+    ldr     sp, =__sp_usr
 
-        @ Switch to Thumb mode
-        add     r0, pc, #1
-        bx      r0
+    @ switch to Thumb
+    add     r0, pc, #1
+    bx      r0
 
-        .thumb
+    .thumb
 
-        @ Clear .sbss section
-        ldr     r0, =__sbss_start
-        mov     r1, #0
-        ldr     r2, =__sbss_size
-        bl      memset32
+    @ clear .bss section
+    ldr     r0, =__bss_start            @ r0 = dest
+    mov     r1, #0                      @ r1 = byte
+    ldr     r2, =__bss_size             @ r2 = n
+    bl      memset32
 
-        @ Clear .bss section
-        ldr     r0, =__bss_start
-        mov     r1, #0
-        ldr     r2, =__bss_size
-        bl      memset32
+    @ clear .sbss section
+    ldr     r0, =__sbss_start           @ r0 = dest
+    mov     r1, #0                      @ r1 = byte
+    ldr     r2, =__sbss_size            @ r2 = n
+    bl      memset32
 
-        @ Copy .data section
-        ldr     r0, =__data_start
-        ldr     r1, =__data_lma
-        ldr     r2, =__data_size
-        bl      memcpy32
+    @ copy .data section
+    ldr     r0, =__data_start           @ r0 = dest
+    ldr     r1, =__data_lma             @ r1 = src
+    ldr     r2, =__data_size            @ r2 = n
+    bl      memcpy32
 
-        @ Copy .ewram section
-        ldr     r0, =__ewram_start
-        ldr     r1, =__ewram_lma
-        ldr     r2, =__ewram_size
-        bl      memcpy32
+    @ copy .iwram section
+    ldr     r0, =__iwram_start          @ r0 = dest
+    ldr     r1, =__iwram_lma            @ r1 = src
+    ldr     r2, =__iwram_size           @ r2 = n
+    bl      memcpy32
 
-        @ Copy .iwram section
-        ldr     r0, =__iwram_start
-        ldr     r1, =__iwram_lma
-        ldr     r2, =__iwram_size
-        bl      memcpy32
+    @ copy .ewram section
+    ldr     r0, =__ewram_start          @ r0 = dest
+    ldr     r1, =__ewram_lma            @ r1 = src
+    ldr     r2, =__ewram_size           @ r2 = n
+    bl      memcpy32
 
-        @ Call AgbMain
-        mov     r0, #0                  @ int argc
-        mov     r1, #0                  @ char *argv[]
+    @ --- call AgbMain --- @
 
-        ldr     r2, =start_vector       @ Set start_vector as return address
-        mov     lr, r2
+    @ set start_vector as return address
+    ldr     r0, =start_vector
+    mov     lr, r0
 
-        ldr     r2, =AgbMain
-        bx      r2
+    ldr     r0, =AgbMain
+    bx      r0
+
+.size start_vector, .-start_vector
 
 .end
