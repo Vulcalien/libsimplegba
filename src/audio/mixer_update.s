57fed6c5d9d18e8da95fa62c214afa1012f07396
Vulcalien <vulcalien@vulcalien.net>
2025-04-20 12:27:39 +0200

    Audio (mixer): optimize sample retrieval in '_mixer_update'
    Instead of keeping the temporary value of position a 12-bit value, keep
    it as a 32-bit value that can go above 0x1000. By doing this, the 'bic'
    instruction in the 'samples loop' can be removed, improving performance.
    
    But also, now (position / 0x1000) cannot be added to the data pointer
    directly. A temporary value 'data_plus_pos' must be used, calculated
    using 'data' as a base pointer and (position / 0x1000) as offset.

--- src/audio/mixer_update.s
+++ src/audio/mixer_update.s
@@ -46,6 +46,7 @@ temp_buffers:
 @   r8  = volume
 @   r9  = temp_buffers
 @   r10 = remaining
+@   r11 = data_plus_pos
 @   r12 = sample
 @   r14 = loop_length
 
@@ -104,12 +105,12 @@ BEGIN_FUNC ARM _mixer_update
 
     ldr     r9, =temp_buffers           @ (r9) temp_buffers
     mov     r10, r2                     @ (r10) remaining = length
+    mov     r11, r4                     @ (r11) data_plus_pos = data
 .L_samples_loop:
     @ retrieve sample and update position
-    ldrsb   r12, [r4]                   @ (r12) sample = *data
+    ldrsb   r12, [r11]                  @ (r12) sample = *data_plus_pos
     add     r6, r7                      @ (r6) position += increment
-    add     r4, r6, lsr #12             @ (r4) data += position / 0x1000
-    bic     r6, #0x000ff000             @ (r6) position &= 0xfff
+    add     r11, r4, r6, lsr #12        @ (r11) data_plus_pos = data + position / 0x1000
 
     @ Add sample to temp_buffers: instead of working on two 16-bit
     @ values separately, work on them combined into a 32-bit value, in a
@@ -136,8 +137,8 @@ BEGIN_FUNC ARM _mixer_update
     add     r14, r12                    @ tmp += sample * volume
     str     r14, [r9], #4               @ *(temp_buffers++) = tmp
 
-    @ if data >= end, loop or stop the channel
-    cmp     r4, r5
+    @ if data_plus_pos >= end, loop or stop the channel
+    cmp     r11, r5
     bge     .L_loop_or_stop
 
 .L_continue_samples_loop:
@@ -145,8 +146,10 @@ BEGIN_FUNC ARM _mixer_update
     bgt     .L_samples_loop             @ if remaining > 0, repeat loop
 .L_exit_samples_loop:
 
-    @ write back data pointer and position
-    str     r4, [r0, #0]                @ channel->data = data
+    @ write data pointer and position
+    str     r11, [r0, #0]               @ channel->data = data_plus_pos
+    lsl     r6, #20
+    lsr     r6, #20                     @ position %= 0x1000
     strh    r6, [r0, #8]                @ channel->position = position
 
 .L_continue_channels_loop:
@@ -200,10 +203,11 @@ BEGIN_FUNC ARM _mixer_update
 
     @ if loop_length != 0, loop channel and continue sample loop
     subne   r4, r5, r14                 @ (r4) data = end - loop_length
+    movne   r11, r4                     @ (r11) data_plus_pos = data
     bne     .L_continue_samples_loop
 
     @ if loop_length == 0, stop channel and break sample loop
-    moveq   r4, #0                      @ (r4) data = NULL
+    moveq   r11, #0                     @ (r11) data_plus_pos = NULL
     beq     .L_exit_samples_loop
 END_FUNC _mixer_update
 
