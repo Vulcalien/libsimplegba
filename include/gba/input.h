2286a4c55121daa9ed981cc89b78c4f6d24e9db8
Vulcalien <vulcalien@vulcalien.net>
2025-05-17 14:07:53 +0200

    Input: add 'input_init' and 'input_repeated' functions
    Using the 'input_repeated' function, it is possible to treat buttons
    held down similarly to how keyboard keys are treated, with repeats after
    a certain delay.

--- include/gba/input.h
+++ include/gba/input.h
@@ -1,4 +1,4 @@
-/* Copyright 2023-2024 Vulcalien
+/* Copyright 2023-2025 Vulcalien
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,11 +7,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #pragma once
 
@@ -30,18 +30,34 @@
 #define KEY_R      BIT(8)
 #define KEY_L      BIT(9)
 
-#define _INPUT_KEY_INPUT   *((vu16 *) 0x04000130)
 #define _INPUT_KEY_CONTROL *((vu16 *) 0x04000132)
-
 #define _INPUT_IRQ_BIT BIT(14)
 
 extern u16 _input_is_down;
 extern u16 _input_was_down;
+extern u16 _input_repeated;
 
-INLINE void input_update(void) {
-    _input_was_down = _input_is_down;
-    _input_is_down  = _INPUT_KEY_INPUT ^ 0x3ff;
+extern u8 _input_repeat_delay;
+extern u8 _input_repeat_interval;
+
+INLINE void input_init(i32 repeat_delay, i32 repeat_interval) {
+    // clip repeat interval in range [1, 240]
+    if(repeat_interval < 1)
+        repeat_interval = 1;
+    if(repeat_interval > 240)
+        repeat_interval = 240;
+
+    // clip repeat delay in range [interval, 240]
+    if(repeat_delay < repeat_interval)
+        repeat_delay = repeat_interval;
+    if(repeat_delay > 240)
+        repeat_delay = 240;
+
+    _input_repeat_delay = repeat_delay;
+    _input_repeat_interval = repeat_interval;
 }
+
+extern void input_update(void);
 
 INLINE bool input_down(u32 key) {
     bool is_down = (_input_is_down & key);
@@ -60,6 +76,11 @@ INLINE bool input_released(u32 key) {
     return !is_down && was_down;
 }
 
+INLINE bool input_repeated(u32 key) {
+    bool is_repeated = (_input_repeated & key);
+    return is_repeated;
+}
+
 // all_keys: if true, all specified keys must be pressed at the same
 //           time to raise the interrupt; if false, any of the specified
 //           keys will raise the interrupt.
@@ -72,7 +93,5 @@ INLINE void input_irq_config(bool all_keys, u32 keys) {
     _INPUT_KEY_CONTROL = val;
 }
 
-#undef _INPUT_KEY_INPUT
 #undef _INPUT_KEY_CONTROL
-
 #undef _INPUT_IRQ_BIT
--- src/input.c
+++ src/input.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 Vulcalien
+/* Copyright 2025 Vulcalien
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,13 +7,46 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #include <gba/input.h>
 
-u16 _input_is_down  = 0;
-u16 _input_was_down = 0;
+#define KEY_INPUT   *((vu16 *) 0x04000130)
+#define KEY_CONTROL *((vu16 *) 0x04000132)
+
+u16 _input_is_down;
+u16 _input_was_down;
+u16 _input_repeated;
+
+u8 _input_repeat_delay;
+u8 _input_repeat_interval;
+
+THUMB
+void input_update(void) {
+    static u8 time_down[10];
+
+    _input_was_down = _input_is_down;
+    _input_is_down  = KEY_INPUT ^ 0x3ff;
+
+    // update repeated status
+    # pragma GCC unroll 0
+    for(u32 i = 0; i < 10; i++) {
+        const u32 key = BIT(i);
+        if(input_down(key))
+            time_down[i]++;
+        else
+            time_down[i] = 0;
+
+        if(time_down[i] == 1 || time_down[i] > _input_repeat_delay)
+            _input_repeated |= key;
+        else
+            _input_repeated &= ~key;
+
+        if(time_down[i] > _input_repeat_delay)
+            time_down[i] -= _input_repeat_interval;
+    }
+}
