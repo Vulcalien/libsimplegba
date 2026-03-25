47e4a6f1593347669da41fd2e26482db296b6d94
Vulcalien <vulcalien@vulcalien.net>
2026-03-26 00:36:56 +0100

    Math: add 'math_udiv' function
    This function returns quotient and remainder of the division between two
    unsigned integers. Given its importance, it is optimized for speed,
    dedicating a significant amount of IWRAM (400 bytes) to it.

---
+++ src/math/udiv.s
@@ -0,0 +1,41 @@
+@ Copyright 2026 Vulcalien
+@
+@ This program is free software: you can redistribute it and/or modify
+@ it under the terms of the GNU General Public License as published by
+@ the Free Software Foundation, either version 3 of the License, or
+@ (at your option) any later version.
+@
+@ This program is distributed in the hope that it will be useful,
+@ but WITHOUT ANY WARRANTY; without even the implied warranty of
+@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+@ GNU General Public License for more details.
+@
+@ You should have received a copy of the GNU General Public License
+@ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+.include "assembly.inc"
+
+@ input:
+@   r0 = num
+@   r1 = den
+@ output:
+@   r0 = quotient
+@   r1 = remainder
+BEGIN_GLOBAL_FUNC .iwram ARM math_udiv
+    mov     r2, #0                      @ (r2) quotient = 0
+.L_loop:
+    .set i, 31
+    .rept 32                            @ for i = 31 to 0:
+        cmp     r1, r0, lsr #i          @   if den <= (num >> i):
+        subls   r0, r0, r1, lsl #i      @     (r0) num -= (den << i)
+        orrls   r2, r2, #(1 << i)       @     (r2) quotient |= BIT(i)
+        .set i, i-1
+    .endr
+
+    @ return quotient and remainder
+    mov     r1, r0                      @ r1 = remainder
+    mov     r0, r2                      @ r0 = quotient
+    bx      lr
+END_FUNC math_udiv
+
+.end
